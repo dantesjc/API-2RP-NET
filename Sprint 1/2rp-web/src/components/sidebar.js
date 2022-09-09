a43b2b5e7fce8c8cc71e0da1a5f1c98654a0333b
ViniciosAlmeida <viniciosalmeida2003@gmail.com>
2022-09-09 16:42:46 -0300

    Agora vai a sidebar

--- Sprint 1/2rp-web/src/components/sidebar.js
+++ Sprint 1/2rp-web/src/components/sidebar.js
@@ -13,10 +13,9 @@ function Sidebar({ children }) {
         <>
 
             <Drawer variant='permanent'>
-<<<<<<< Updated upstream
-                <Box width={theme.spacing(30)} height={theme.spacing(100)} className ='side-bar'>
+                <Box width={theme.spacing(31)} height={theme.spacing(100)} className ='side-bar'>
                       <div className='texto'>
-                        <ul >
+                        
                             <br />
                             <br />
                             <br />
@@ -25,46 +24,22 @@ function Sidebar({ children }) {
                                     &nbsp;
                                     &nbsp;
                                     &nbsp;
+                                    &nbsp;
                                     Dashboard
                                 </Link>
                                 <br />
                                 <br />
                                 <Link to='/horaextra' className ='side-links'>
+                                    &nbsp;
                                     Apontar Hora Extra
                                 </Link>
                                 <br />
                                 <br />
                                 <Link to='/sobreaviso' className ='side-links'>
+                                    &nbsp;
                                     Apontar sobre aviso
                                 </Link>
-                        </ul>
-=======
-                <Box width={theme.spacing(31)} height={theme.spacing(100)} className ='side-bar'>
-                    <div className='texto'>
-                        <br />
-                        <br />
-                        <br />
-                        <br />
-                        <Link to='/dashboard' className ='side-links'>
-                            &nbsp;
-                            &nbsp;
-                            &nbsp;
-                            &nbsp;
-                            Dashboard
-                        </Link>
-                        <br />                                
-                        <br />
-                        <Link to='/horaextra' className ='side-links'>
-                            &nbsp;
-                            Apontar Hora Extra
-                        </Link>
-                        <br />
-                        <br />
-                        <Link to='/sobreaviso' className ='side-links'>
-                            &nbsp;
-                            Apontar sobre aviso
-                        </Link>
->>>>>>> Stashed changes
+                        
                     </div>
                     <div>
                         <br />
@@ -84,11 +59,7 @@ function Sidebar({ children }) {
                     </div>
                 </Box>
             </Drawer>
-<<<<<<< Updated upstream
-            <Box height="100vh" marginLeft={theme.spacing(30)}>
-=======
             <Box height="100vh" marginLeft={theme.spacing(31)}>
->>>>>>> Stashed changes
 
                 {children}
             </Box>
