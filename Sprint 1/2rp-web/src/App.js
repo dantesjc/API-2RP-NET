c647e056b4d9cd33cd6d5a4f59283bbf80347664
ViniciosAlmeida <viniciosalmeida2003@gmail.com>
2022-09-13 16:44:27 -0300

    Início do Sobre aviso

--- Sprint 1/2rp-web/src/App.js
+++ Sprint 1/2rp-web/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import Login from './pages/login/login'
 import Dashboard from './pages/home/dashboard'
+import Sobreaviso from './pages/home/sobreaviso'
 
 import Button from 'react-bootstrap'
 
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path='/login' element={<Login />} />
       <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/sobreaviso' element={<Sobreaviso />} />
     </Routes>
 
   );
