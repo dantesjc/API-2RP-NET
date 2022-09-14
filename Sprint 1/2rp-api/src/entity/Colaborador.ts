c3bda1084a6eee5d31c369fb202b7d18840ff617
fred <fred.rbo23@gmail.com>
2022-09-13 21:06:53 -0300

    criando entidade colab e hora extra

---
+++ Sprint 1/2rp-api/src/entity/Colaborador.ts
@@ -0,0 +1,20 @@
+import { perfil } from './../enum/perfil-enum';
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity({ name: "colaborador" })
+export class Colaborador {
+    @PrimaryGeneratedColumn("uuid")
+    id_colaborador: string
+
+    @Column({ nullable: false, unique: true })
+    matricula: number
+
+    @Column({ nullable: false, unique: true })
+    senha: number
+
+    @Column({ length: 70, nullable: false, unique: true })
+    nome: string
+
+    @Column({ nullable: false, select: false })
+    perfil: string
+}
