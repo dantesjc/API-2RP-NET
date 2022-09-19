92e02af6f47a410d020e05e977fa99fda16314a5
fred <fred.rbo23@gmail.com>
2022-09-19 10:12:15 -0300

    adicionando snack

--- Sprint 1/2rp-web/src/pages/home/sobreaviso/sobreaviso.js
+++ Sprint 1/2rp-web/src/pages/home/sobreaviso/sobreaviso.js
@@ -19,18 +19,24 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
 
 import './sobreaviso.css'
 import { createBreakpoints } from "@mui/system";
 
-function HoraExtra() {
+function Sobreaviso() {
     const [value, setValue] = useState({
-        code: '',
+        code: '3016',
         status: 'Pendente'
     });
     const [selectedDate, setDate] = useState(dayjs(new Date()))
     const [selectedStart, setStart] = useState(dayjs(new Date()))
     const [selectedEnd, setEnd] = useState(dayjs(new Date()))
+    const [open, setOpen] = React.useState(false);
+    const [textSnack, setTextSnack] = React.useState('');
+    const [colorSnack, setColorSnack] = React.useState('');
 
     const handleChange = (value) => {
         setValue(prevValue => ({
@@ -39,34 +45,72 @@ function HoraExtra() {
         }));
     };
 
+    const Alert = React.forwardRef(function Alert(props, ref) {
+        return <MuiAlert elevation={10} ref={ref} variant="filled" {...props} />;
+      });
+      
 
+    const openSnack = (text, color) => {
+        setOpen(true);
+        setTextSnack(text)
+        setColorSnack(color)
+    };
 
-    let formData = {
+    const closeSnack = (event , reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        setOpen(false);
+      };
+
+    const [form, setForm] = useState({
         code: '',
         data: '',
         start: '',
         end: '',
-        status: ''
-    }
+        status: '',
+        delete: ''
+    })
 
-    const setFormData = (form) => {
-        form.code = value.code
-        form.data = selectedDate.$d.getDate() + "/" + (selectedDate.$d.getMonth() + 1) + "/" + selectedDate.$d.getFullYear();
-        form.start = selectedStart.$d.getHours() + ':' + selectedStart.$d.getMinutes()
-        form.end = selectedEnd.$d.getHours() + ':' + selectedEnd.$d.getMinutes()
-        form.status = value.status
-        
-    }
-
-    const submit = (form) => {
-        setFormData(form);
-        console.log(form)
+    const resetForm = () => {
+        setValue('')
 
     }
+
+    const setFormData = () => {
+        setForm({
+            code: value.code,
+            data: selectedDate.$d.getDate() + "/" + (selectedDate.$d.getMonth() + 1) + "/" + selectedDate.$d.getFullYear(),
+            start: selectedStart.$d.getHours() + ':' + selectedStart.$d.getMinutes(),
+            end: selectedEnd.$d.getHours() + ':' + selectedEnd.$d.getMinutes(),
+            status: "Pendente",
+            delete: <button className="ml-4" onClick={deleteForm}><i class="bi bi-trash3"></i></button>
+        })
+        resetForm();
+    }
+    const deleteForm = () => {
+        setForm({
+            code: '',
+            data: '',
+            start: '',
+            end: '',
+            status: '',
+            delete: ''
+        })
+
+        openSnack('Deletado com sucesso', 'error')
+    }
+
+    const submit = () => {
+        openSnack('Sobreaviso cadastrada com sucesso', 'success')
+        setFormData();
+    }
+
 
     return (
         <Sidebar>
-            <Header nome="Apontar sobreaviso"></Header>
+            <Header nome="Apontar Sobreaviso"></Header>
             {/* linha 1 data, inicio, fim */}
             <div className="d-flex flex-column align-items-center">
                 <div className="row justify-content-center align-items-center col-md-10 mt-4">
@@ -80,12 +124,10 @@ function HoraExtra() {
                                     id="demo-simple-select"
                                     label="Código verba"
                                     name="code"
+                                    readonly disabled
                                     value={value.code}
                                     onChange={handleChange}>
-                                    <MenuItem value={1601}>1601</MenuItem>
-                                    <MenuItem value={1602}>1602</MenuItem>
-                                    <MenuItem value={1809}>1809</MenuItem>
-                                    <MenuItem value={3000}>3000</MenuItem>
+                                    <MenuItem value={3016}>3016</MenuItem>
                                 </Select>
                             </FormControl>
                         </Box>
@@ -122,7 +164,8 @@ function HoraExtra() {
                     <div className="col-md-2 col-sm-10 mt-3">
                         <LocalizationProvider dateAdapter={AdapterDayjs}>
                             <Stack spacing={3}>
-                                <TimePicker readOnly disabled
+                                <TimePicker
+                                    // <TimePicker readOnly disabled
                                     label="Fim"
                                     value={selectedEnd}
                                     onChange={end => setEnd(end)}
@@ -136,10 +179,11 @@ function HoraExtra() {
                     <div className="col-3 mt-3">
                         <Button variant="success"
                             disabled={value.code == ''}
-                            onClick={() => submit(formData)
+                            onClick={() => submit()
                             }>Confirmar</Button>
                     </div>
                 </div>
+
 
                 {/* tabela */}
                 <div className="row justify-content-center  col-md-10  mt-5">
@@ -147,27 +191,35 @@ function HoraExtra() {
                     <Table striped bordered hover responsive >
                         <thead >
                             <tr>
-                                <th className="col-md-2">Código Verba</th>
+                                <th className="col-md-2 ">Código Verba</th>
                                 <th className="col-md-2">Data</th>
                                 <th className="col-md-2">Hora Início</th>
                                 <th className="col-md-2">Hora fim</th>
                                 <th className="col-md-2">Status</th>
+                                <th className="col-md-1">Ações</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td>{value.code}</td>
-                                <td>{formData.data}</td>
-                                <td>{formData.start}</td>
-                                <td>{formData.end}</td>
-                                <td>{formData.status}</td>
+                                <td>{form.code}</td>
+                                <td>{form.data}</td>
+                                <td>{form.start}</td>
+                                <td>{form.end}</td>
+                                <td>{form.status}</td>
+                                <td>{form.delete}</td>
                             </tr>
                         </tbody>
                     </Table>
                 </div>
             </div>
+            <Snackbar open={open} autoHideDuration={3000} onClose={closeSnack}>
+                <Alert onClose={closeSnack} severity={colorSnack} sx={{ width: '100%' }}>
+                    {textSnack}
+                </Alert>
+            </Snackbar>
         </Sidebar >
     )
 }
 
-export default HoraExtra
+
+export default Sobreaviso
