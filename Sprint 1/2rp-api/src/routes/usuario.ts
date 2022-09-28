1d8d9b1fdfeafd9ba47cf96b43513ad4530abb72
fredrbo <fred.rbo23@gmail.com>
2022-09-27 21:16:33 -0300

    apenas usuario

--- Sprint 1/2rp-api/src/routes/usuario.ts
+++ Sprint 1/2rp-api/src/routes/usuario.ts
@@ -4,7 +4,6 @@ import UsuarioController from "../controllers/UsuarioController";
 const routes = Router();
 
 routes.get('/', UsuarioController.find)
-// routes.get('/getAll', UsuarioController.getAll)
 routes.post('/', UsuarioController.create)
 routes.put('/', UsuarioController.update)
 routes.delete('/', UsuarioController.delete)
