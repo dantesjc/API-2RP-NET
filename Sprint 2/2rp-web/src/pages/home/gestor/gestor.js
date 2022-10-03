f9b87fe8cf7a09a3fedd07bcccdf98d20e66dacc
fred <fred.rbo23@gmail.com>
2022-10-03 10:06:31 -0300

    rota gestor

--- Sprint 2/2rp-web/src/pages/home/gestor/gestor.js
+++ Sprint 2/2rp-web/src/pages/home/gestor/gestor.js
@@ -16,54 +16,53 @@ class MyLabel extends Component {
 }
 
 
-
 function Gestor() {
-    return (
-        <Sidebar>
-          <Header nome="Horas Extras"></Header>
+  return (
+      <Sidebar>
+        <Header nome="Horas Extras"></Header>
 
-          {/* linha 1 */}
-          <div className="background d-flex flex-column aling-itens-center">
-            
-            <div>
-            <span className="soli">
-            <label className="font">Solicitações hora extra</label> 
-            </span>
+        {/* linha 1 */}
+        <div className="background d-flex flex-column aling-itens-center">
+          
+          <div>
+          <span className="soli">
+          <label className="font">Solicitações hora extra</label> 
+          </span>
 
-            <span className="filtroLabel">
-            <label className="font">Filtro</label> <input className="mt-4 mb-4 ml-2"></input>
-            </span>
+          <span className="filtroLabel">
+          <label className="font">Filtro</label> <input className="mt-4 mb-4 ml-2"></input>
+          </span>
 
-            {/* card info hora extra */}
-            <div className="d-flex flex-column aling-itens-center">
-            <Card 
-              Funcionario = "Carlos Blaster"
-              Data = "25/10/11"
-              HoraInicial = "11:00"
-              HoraFinal = "17:00"
-            />
+          {/* card info hora extra */}
+          <div className="d-flex flex-column aling-itens-center">
+          <Card 
+            Funcionario = "Carlos Blaster"
+            Data = "25/10/11"
+            HoraInicial = "11:00"
+            HoraFinal = "17:00"
+          />
 
-            <Card 
-              Funcionario = "Carlos Magno"
-              Data = "26/10/11"
-              HoraInicial = "12:00"
-              HoraFinal = "18:00"
-            />
+          <Card 
+            Funcionario = "Carlos Magno"
+            Data = "26/10/11"
+            HoraInicial = "12:00"
+            HoraFinal = "18:00"
+          />
 
-            <Card 
-              Funcionario = "Carlos Magno"
-              Data = "26/10/11"
-              HoraInicial = "12:00"
-              HoraFinal = "18:00"
-            />
-            </div>
-            
-
-            </div>
-              
+          <Card 
+            Funcionario = "Carlos Magno"
+            Data = "26/10/11"
+            HoraInicial = "12:00"
+            HoraFinal = "18:00"
+          />
           </div>
-        </Sidebar>
-    )
+          
+
+          </div>
+            
+        </div>
+      </Sidebar>
+  )
 }
 
 export default Gestor
